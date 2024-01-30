cbe31afcf89ee35fb5bb602935db3ae261147faa
Rommmu <tonty1222@naver.com>
2024-01-30 23:39:28 +0900

    refactor: DTO

--- firstproject/src/main/java/com/example/firstproject/dto/ArticleDto.java
+++ firstproject/src/main/java/com/example/firstproject/dto/ArticleDto.java
@@ -1,23 +1,14 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleDto {
     private String title;
     private String content;
-
-    public ArticleDto(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null, title, content);
